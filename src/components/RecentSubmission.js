bf40c8e057fe83691cd0c62a6c071c19e75b5c73
kate myer <kate.d.mangubat@gmail.com>
2020-04-22 18:50:07 -0700

    wave 2 recent subs

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './RecentSubmission.css';
 
 const RecentSubmission = (props) => {
+  //Props
+
+
+  //check if poem exists by checking the hash
+  let poemHtml = '';
+  if (typeof props.recentPoem !== "undefined" && props.recentPoem.hasOwnProperty('adj1')) { 
+    //true, then display the recent submission
+    poemHtml = `The ${props.recentPoem.adj1} ${props.recentPoem.noun1} ${props.recentPoem.adv} ${props.recentPoem.verb} the ${props.recentPoem.adj2} ${props.recentPoem.noun2}.`
+  } 
+  
+//HTML
   return (
     <div className="RecentSubmission">
+      
       <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{  }</p>
+      <p className="RecentSubmission__submission">{poemHtml}</p>
     </div>
   );
 }
