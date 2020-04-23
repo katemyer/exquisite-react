c569b769531620def6b4a42dfe991909a0493660
kate myer <kate.d.mangubat@gmail.com>
2020-04-22 18:49:16 -0700

    wave 2

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -2,14 +2,35 @@ import React from 'react';
 import './FinalPoem.css';
 
 const FinalPoem = (props) => {
+  // PROPS
+  //  <FinalPoem 
+  //   // PROPS
+  //   poemList={poemList}
+  //  callback={call}
+  // />
+  // props.poemList
+  // props.callback
+
+    const poemsHtml = props.poemList.map((poem, i) => {
+      return (
+        <li key={i}> 
+          The {poem.adj1} {poem.noun1} {poem.adv} {poem.verb} the {poem.adj2} {poem.noun2}.
+        </li>
+      );
+    });
+  
+    console.log(poemsHtml)
 
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
+        
 
       </section>
-
+      <div> 
+       <ul className="student-collection">{poemsHtml}</ul>
+      </div>
       <div className="FinalPoem__reveal-btn-container">
         <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
       </div>
