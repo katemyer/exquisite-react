ed68dbed37c651cc9406d7efe0a04b30fb313250
kate myer <kate.d.mangubat@gmail.com>
2020-04-22 23:33:33 -0700

    Updates

---
+++ precache-manifest.788533ee67e344afde441552f7067be8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f09314c04d9035873746fb1024a5e41",
+    "url": "/exquisite-react/index.html"
+  },
+  {
+    "revision": "04fc8857c662db1eb6b2",
+    "url": "/exquisite-react/static/css/main.8bd81cdd.chunk.css"
+  },
+  {
+    "revision": "908eb79a920c7d6a3bbc",
+    "url": "/exquisite-react/static/js/2.fed5983e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exquisite-react/static/js/2.fed5983e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fc8857c662db1eb6b2",
+    "url": "/exquisite-react/static/js/main.52a4140e.chunk.js"
+  },
+  {
+    "revision": "5ae0c249b5fa1be938c0",
+    "url": "/exquisite-react/static/js/runtime-main.b27f8108.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/exquisite-react/precache-manifest.980aaa0a94eb36e4e50e2a0f40d9f30a.js"
+  "/exquisite-react/precache-manifest.788533ee67e344afde441552f7067be8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/exquisite-react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
